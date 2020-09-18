f3d4e5b499556a0ab329d30b132b73b9c08655f1
tdyleuth <tony.dyleuth@gmail.com>
2020-09-17 19:43:32 -0700

    Adding icons and button in sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -10,12 +10,13 @@ import ListAltIcon from "@material-ui/icons/ListAlt";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import PermIdentityIcon from "@material-ui/icons/PermIdentity";
 import SidebarOption from "./SidebarOption";
+import { Button } from "@material-ui/core";
 
 function Sidebar() {
     return (
         <div className='sidebar'>
             <TwitterIcon />
-            <SidebarOption Icon={HomeIcon} text='Home' />
+            <SidebarOption active Icon={HomeIcon} text='Home' />
             <SidebarOption Icon={SearchIcon} text='Explore' />
             <SidebarOption Icon={NotificationsNoneIcon} text='Notifications' />
             <SidebarOption Icon={MailOutlineIcon} text='Messages' />
@@ -23,6 +24,10 @@ function Sidebar() {
             <SidebarOption Icon={ListAltIcon} text='lists' />
             <SidebarOption Icon={PermIdentityIcon} text='Profile' />
             <SidebarOption Icon={MoreHorizIcon} text='More' />
+
+            <Button variant='outlined' className='sidebar__tweet' fullWidth>
+                Tweet
+            </Button>
         </div>
     );
 }
