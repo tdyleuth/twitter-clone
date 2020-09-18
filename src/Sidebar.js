299c267ae59f7114c058a434c5532425acef99ed
tdyleuth <tony.dyleuth@gmail.com>
2020-09-17 19:00:34 -0700

    Init Twitter Clone React App

---
+++ src/Sidebar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Sidebar.css";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import ListAltIcon from "@material-ui/icons/ListAlt";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import PermIdentityIcon from "@material-ui/icons/PermIdentity";
+import SidebarOption from "./SidebarOption";
+
+function Sidebar() {
+    return (
+        <div className='sidebar'>
+            <TwitterIcon />
+            <SidebarOption Icon={HomeIcon} text='Home' />
+            <SidebarOption Icon={SearchIcon} text='Explore' />
+            <SidebarOption Icon={NotificationsNoneIcon} text='Notifications' />
+            <SidebarOption Icon={MailOutlineIcon} text='Messages' />
+            <SidebarOption Icon={BookmarkBorderIcon} text='Bookmarks' />
+            <SidebarOption Icon={ListAltIcon} text='lists' />
+            <SidebarOption Icon={PermIdentityIcon} text='Profile' />
+            <SidebarOption Icon={MoreHorizIcon} text='More' />
+        </div>
+    );
+}
+
+export default Sidebar;
