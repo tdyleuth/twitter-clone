bd00c437549dbd717edb1acd775d8ea342dadd04
tdyleuth <tony.dyleuth@gmail.com>
2020-09-22 20:10:05 -0700

    Adding screenshot

--- src/Feed.js
+++ src/Feed.js
@@ -3,6 +3,7 @@ import "./Feed.css";
 import Post from "./Post";
 import TweetBox from "./TweetBox";
 import db from "./firebase";
+import FlipMove from "react-flip-move";
 
 function Feed() {
     const [posts, setPosts] = useState([]);
@@ -19,17 +20,20 @@ function Feed() {
             <div className='feed__header'>
                 <h2>Home</h2>
             </div>
-            <TweetBox />
-            {posts.map((post) => (
-                <Post
-                    displayName={post.displayName}
-                    username={post.username}
-                    verified={post.verified}
-                    text={post.text}
-                    avatar={post.avatar}
-                    image={post.image}
-                />
-            ))}
+            <TweetBox />{" "}
+            <FlipMove>
+                {posts.map((post) => (
+                    <Post
+                        key={post.text}
+                        displayName={post.displayName}
+                        username={post.username}
+                        verified={post.verified}
+                        text={post.text}
+                        avatar={post.avatar}
+                        image={post.image}
+                    />
+                ))}
+            </FlipMove>
         </div>
     );
 }
